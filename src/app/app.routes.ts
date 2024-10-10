a5e930ad4dbf26c2d4ecfa09d0ee00e789a771e6
DOVER-GLOBAL\11049921 <kime@icscarwashsystems.com>
2024-10-10 15:09:06 -0400

    Initial routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'products',
+        component: ProductsComponent
+    },
+    {
+        path:'checkout',
+        component: CheckoutComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
