ce8136eac987008dbcd3ed7d1f26179153853e2d
DOVER-GLOBAL\11049921 <kime@icscarwashsystems.com>
2024-10-10 14:51:23 -0400

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'EcommerceWebsite';
+  registerObj: any = {
+    "CustId": 0,
+    "Name": "",
+    "MobileNo": "",
+    "Password": ""
+  }
+  loginObj: any = {
+    "UserName": "",
+    "UserPassword": ""
+  }
+  loggedObj: any = {};
+  cartItems: any[]= [];
+  loginModelClass: string = '';
 }
